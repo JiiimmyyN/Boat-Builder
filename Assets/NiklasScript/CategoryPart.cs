750b98b6ac9cd576646c3ae035c36379858fe2d2
chimchim <nicke_lin_91@hotmail.com>
2017-11-22 16:15:28 +0100

    Add Customize Menu

---
+++ Assets/NiklasScript/CategoryPart.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CategoryPart : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData pointerEventData)
+	{
+		_customize.ActivateCollection(gameObject, Pieces);
+    }
+
+	public GameObject Item;
+	public List<GameObject> Pieces;
+
+	private Customize _customize;
+	public void Start()
+	{
+    }
+
+	public void SetPieces(List<GameObject> pieces)
+	{
+		Pieces = pieces;
+    }
+
+	public void SetAsset(GameObject go)
+	{
+		_customize = FindObjectOfType<Customize>();
+		Item = go;
+		var image = GetComponent<Image>();
+		if (image != null)
+		{
+			var texture = AssetPreview.GetAssetPreview(go);
+			Rect rec = new Rect(0, 0, texture.width, texture.height);
+			var sprite = Sprite.Create(texture, rec, new Vector2(0, 0));
+			image.sprite = sprite;
+		}
+	}
+}
