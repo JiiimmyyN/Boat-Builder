bac7c6ab491222c08dbc40f7bdca931bf9d99de0
Jimmy Nilsson <Jiiiimmmyy@gmail.com>
2017-11-22 20:02:13 +0100

    Fix niklas shit

--- Assets/NiklasScript/CategoryPart.cs
+++ Assets/NiklasScript/CategoryPart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -27,17 +28,24 @@ public class CategoryPart : MonoBehaviour
 
 	public void SetAsset(GameObject go)
 	{
-		_customize = FindObjectOfType<Customize>();
-		Item = go;
-		var image = GetComponent<Image>();
-		if (image != null)
+		try
 		{
-			image.sprite = null;
-			var texture = AssetPreview.GetAssetPreview(go);
-            Rect rec = new Rect(0, 0, texture.width, texture.height);
-			var sprite = Sprite.Create(texture, rec, new Vector2(0, 0));
-			image.sprite = sprite;
-			
-        }
+			_customize = FindObjectOfType<Customize>();
+			Item = go;
+			var image = GetComponent<Image>();
+			if (image != null)
+			{
+				image.sprite = null;
+				var texture = AssetPreview.GetAssetPreview(go);
+				Rect rec = new Rect(0, 0, texture.width, texture.height);
+				var sprite = Sprite.Create(texture, rec, new Vector2(0, 0));
+				image.sprite = sprite;
+				
+			}
+		}
+		catch(Exception e)
+		{
+			Debug.LogException(e);
+		}
 	}
 }
